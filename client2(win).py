37b88ccb42135dc6dadc11c2509dc93d737180a6
anmolroop00 <anmol.roop00@gmail.com>
2021-01-11 00:14:13 +0530

    Windows new client

---
+++ client2(win).py
@@ -0,0 +1,23 @@
+import socket
+import threading
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(("192.168.1.7",1234))
+print("Server Started")
+def sender():
+    while True:
+        message = input()
+        s.sendto(message.encode(), ("192.168.1.43", 1234))
+def receiver():
+	while True:
+		x = s.recvfrom(1024)
+		y=x[1][0]
+		x=x[0].decode()
+		print("\t\t{0}:{1}".format(y,x))            
+
+
+x1 = threading.Thread(target=sender)
+x2 = threading.Thread(target=receiver)
+
+x1.start()
+x2.start()
